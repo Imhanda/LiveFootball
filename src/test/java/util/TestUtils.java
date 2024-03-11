38453944d6083b3a468101fb6b7e3ac9062b397f
Imhanda <vipinhanda2013@gmail.com>
2024-03-11 18:49:02 +0900

    Update tests
    - Rename test case
    - Move get resource method to util class
    - Removing print statement
    - Adding time delay for starting matches for test case: testScoreboardSummaryWithEndedMatch()

---
+++ src/test/java/util/TestUtils.java
@@ -0,0 +1,22 @@
+package util;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class TestUtils {
+
+    /**
+     * Fetch JSON String from resources file
+     *
+     * @param fileName
+     * @return JSON String
+     */
+    public static String getExpectedScoreboard(String fileName) throws IOException {
+        File file = new File(Objects.requireNonNull(TestUtils.class.getClassLoader().getResource(fileName)).getFile());
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+    }
+}
